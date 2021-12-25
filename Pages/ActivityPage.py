c70bbb7d8a26b218ca619350737000d4aa3ef66b
badlydeadly <arif4208@gmail.com>
2021-12-25 10:45:39 +0600

    Update : ActivityPage.py, test_ActivityPage.py, test_HomePage.py
    Add: buy_nft.py

--- Pages/ActivityPage.py
+++ Pages/ActivityPage.py
@@ -6,14 +6,23 @@ from Pages.BasePage import BasePage
 
 class ActivityPage(BasePage):
     ACTIVITY_URL = "https://opensea.io/activity?search[collections][0]=clonex&search[collections][" \
-           "1]=boredapeyachtclub&search[collections][2]=dinobabies&search[collections][3]=coolmans-universe&search[" \
-           "eventTypes][0]=AUCTION_CREATED "
+                   "1]=boredapeyachtclub&search[collections][2]=dinobabies&search[collections][3]=coolmans-universe&search[" \
+                   "eventTypes][0]=AUCTION_CREATED "
     ACTIVITY_TITLE = "Activity | OpenSea"
+
+    NFT_NAMES = (By.XPATH, "//div[@role='listitem']//child::div[@class='AssetCell--container']")
+
+    NFT_PRICES = (By.XPATH, "/div[@class='Overflowreact__OverflowContainer-sc-10mm0lu-0 gjwKJf Price--fiat-amount']")
 
     def __init__(self, driver):
         super().__init__(driver)
         self.driver.get(self.ACTIVITY_URL)
 
+    def get_first_nft_name(self, NFT_NAMES):
+        self.get_element_text(NFT_NAMES)
 
 
-
+    # def get_first_nft_price(self, driver):
+    #     nft_prices = driver.find_elements_by_xpath(self.NFT_PRICE_XPATH)
+    #     nft_price = nft_prices[-1].text
+    #     return nft_price
--- Tests/test_ActivityPage.py
+++ Tests/test_ActivityPage.py
@@ -1,6 +1,7 @@
 from Config.config import TestData
 from Pages.HomePage import HomePage
 from Tests.test_BasePage import BaseTest
+
 from Pages.ActivityPage import ActivityPage
 
 
@@ -10,6 +11,11 @@ class TestActivityPage(BaseTest):
         title = self.activity_page.get_title(ActivityPage.ACTIVITY_TITLE)
         assert title == ActivityPage.ACTIVITY_TITLE
 
+    def test_get_first_nft_name(self):
+        self.activity_page = ActivityPage(self.driver)
+        self.activity_page.get_first_nft_name(ActivityPage.NFT_NAMES)
 
-
-    # another test
+    # def test_get_first_nft_price(self):
+    #     self.activity_page = ActivityPage(self.driver)
+    #     first_nft_price = self.activity_page.get_first_nft_price(self.driver)
+    #     print(first_nft_price)
