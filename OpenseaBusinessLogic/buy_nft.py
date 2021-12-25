4e0a51f2598674b1994d0ba854947f11360fd141
Sushen Biswas <sushenbiswasaga@gmail.com>
2021-12-25 10:57:18 +0600

    Update: buy-nft.py

---
+++ OpenseaBusinessLogic/buy_nft.py
@@ -0,0 +1,21 @@
+# TODO : Go to Base Website - https://opensea.io/
+
+# TODO : Login Using Meta Mask and Stop Script
+
+# TODO : Find NFT Name
+
+# TODO : Find NFT Price
+
+# TODO : Find NFT Floor Price
+
+# TODO : Compare Floor Price to NFT present Price
+
+# TODO : Repeat this process using loop
+
+# TODO : GO for Single NFT Page
+
+# TODO : Buy Single NFT
+
+# TODO : Start The repeat loop and Continue...
+
+
