8f00b90ffd18633f61be2926d93ec09debb3851d
Sushen Biswas <sushenbiswasaga@gmail.com>
2022-01-04 02:17:08 -0600

    Update: buy-nft.py

--- buy-nft.py
+++ buy-nft.py
@@ -45,7 +45,9 @@ print("Script started at - " + time.ctime())
 
 for i in range(100):
 
-    # TODO : When we find place bit what we are going to do - link example https://opensea.io/assets/0xf9d53e156fe880889e777392585feb46d8d840f6/4745
+    # TODO : When we find place bit what we are going to do - link example
+    #  https://opensea.io/assets/0xf9d53e156fe880889e777392585feb46d8d840f6/4745
+
     # TODO : Excellent opportunity for storing ndt link to database so later we can buy this NFT
     loop_run_time = (time.time() - start_time)
     print(f"This script was running for {loop_run_time}")
